c56fea4a3161d7f2278c7692303bdfafae743aef
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-10-09 19:59:02 +0200

    Dodanie menu i styli

--- src/TicketTracker/Domain/Models/Ticket.cs
+++ src/TicketTracker/Domain/Models/Ticket.cs
@@ -3,9 +3,10 @@ public class TicketSearchCriteria  : Base
 {
      public string? Title { get; set; }
     public string? Description { get; set; }
-     public TicketStatus? Status { get; set; }
+     public TicketStatus Status { get; set; }
      public DateTime? From { get; set; }
      public DateTime? To { get; set; }
+     
 }
 
 public partial class Ticket : BaseEntity
@@ -15,6 +16,11 @@ public partial class Ticket : BaseEntity
     public DateTime CreatedOn { get; set; }
     public string CreatedBy { get; set; }
     public TicketStatus Status { get; set; }
+
+    public Ticket()
+    {
+        Status = TicketStatus.New;
+    }
 }
 
 public enum TicketStatus
