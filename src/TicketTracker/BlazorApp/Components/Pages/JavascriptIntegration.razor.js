2eafbce265d17902c750c861c448cbae08ad0317
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-10-25 10:49:53 +0200

    Dodanie izolacji js i uruchamiania funkcji js na podstawie zdarzeń strony

---
+++ src/TicketTracker/BlazorApp/Components/Pages/JavascriptIntegration.razor.js
@@ -0,0 +1,14 @@
+﻿
+// dotnet add package BlazorPageScript
+
+export function onLoad() {
+    console.log("loaded.");
+}
+
+export function onUpdate() {
+    console.log("updated.");
+}
+
+export function onDispose() {
+    console.log("Dispose.");
+}
